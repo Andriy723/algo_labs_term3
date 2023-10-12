ff760d1ee64da87c8cd564abfc0f9d4512755b22
Andriy723 <113835398+Andriy723@users.noreply.github.com>
2023-10-12 21:53:50 +0300

    divide file on test and main files

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import unittest
-
-
 class BinaryTree:
     def __init__(self, value, left=None, right=None):
         self.value = value
@@ -9,7 +6,7 @@ class BinaryTree:
 
     def height(self, node):
         if node is None:
-            return 1
+            return 0
 
         height_left = self.height(node.left)
         height_right = self.height(node.right)
@@ -31,32 +28,3 @@ class BinaryTree:
             return False
         else:
             return True
-
-class Test(unittest.TestCase):
-    def test_equal_1(self):
-
-        root = BinaryTree(3)
-        root.left = BinaryTree(9)
-        root.right = BinaryTree(20)
-
-        self.assertTrue(root.is_tree_balanced(root))
-
-    def test_equal_2(self):
-
-        root = BinaryTree(1)
-        root.left = BinaryTree(12)
-        root.right = BinaryTree(25)
-        root.left.left = BinaryTree(10)
-        root.left.left.left = BinaryTree(5)
-
-        self.assertFalse(root.is_tree_balanced(root))
-
-    def test_equal_3(self):
-
-        root = BinaryTree(111)
-        root.left = BinaryTree(121)
-        root.right = BinaryTree(156)
-        root.right.right = BinaryTree(200)
-        root.right.right.right = BinaryTree(250)
-
-        self.assertFalse(root.is_tree_balanced(root))
